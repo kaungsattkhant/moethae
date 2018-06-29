39c6b3db8c831549024e5062d20f07cd419b1cab
kaungsattkhant <40640520+kaungsattkhant@users.noreply.github.com>
2018-06-29 12:35:50 +0630

    Update cat-del.php
    del

--- cat-del.php
+++ cat-del.php
@@ -3,6 +3,6 @@ include("confs/config.php");
 
 $id=$_GET['id'];
 $sql= "DELETE FROM category WHERE id=$id" ;
-mysqli_query($conn,$sql);
+mysqli_query($conn,$sql) or die("cannot");
 header("location: cat-list.php");
 ?>
